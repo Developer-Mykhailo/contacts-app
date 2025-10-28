ac8122a8dca854e3b504390aaa41a726da3b2c5c
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-10-28 08:12:32 -0600

    filter

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -8,17 +8,20 @@ import {
 } from '../services/students.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 //---------------------------------------------------------------
 export const getStudentsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortOrder, sortBy } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
   const students = await getAllStudents({
     page,
     perPage,
     sortBy,
     sortOrder,
+    filter,
   });
 
   res.json({
