3b4335e0cf75e6727345beebd10e22ea9b0e3854
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-10-29 01:49:56 -0600

    cookie

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import studentsRouter from './routers/students.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { getApiInfo } from './utils/readApiInfo.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -14,6 +15,7 @@ export const startServer = async () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
